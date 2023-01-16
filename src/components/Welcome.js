17d74f4c7fc3125323e5a580485726cc13330d06
TuyishimireEric <tuyishimireericc@gmail.com>
2023-01-16 16:48:33 +0200

    Updated the emails link

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -19,7 +19,7 @@ function Welcome() {
         <ul className="social">
           <li><a href="https://github.com/TuyishimireEric" target="_blank" rel="noopener noreferrer"><img src={github} alt="github" /></a></li>
           <li><a href="https://www.linkedin.com/in/tuyishimireeric/" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="github" /></a></li>
-          <li><a href="mailto:email@example.com?subject=portfolio" target="_blank" rel="noopener noreferrer"><img src={gmail} alt="github" /></a></li>
+          <li><a href="mailto:tuyishimireericc@gmail.com?subject=portfolio" target="_blank" rel="noopener noreferrer"><img src={gmail} alt="github" /></a></li>
           <li><a href="https://twitter.com/TuyishimirEric?t=2eZF5gQGecCpfZW-gEoDOw&s=08" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="github" /></a></li>
           <li><a href="https://angel.co/u/tuyishimire-eric" target="_blank" rel="noopener noreferrer"><img src={angelist} alt="github" /></a></li>
         </ul>
